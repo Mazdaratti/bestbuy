649ebbf797b65a6e19b9e18b60cd265aaaa217f4
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-10-24 00:07:41 +0200

    Update main.py
    New initialization code and new imports

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import products
 import store
+import promotions
 
 
 def display_menu():
@@ -42,11 +43,22 @@ def main():
     """
     Main function to initialize the store and start the program.
     """
-    product_list = [
-        products.Product("MacBook Air M2", price=1450, quantity=100),
-        products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-        products.Product("Google Pixel 7", price=500, quantity=250)
-    ]
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250),
+                    products.NonStockedProduct("Windows License", price=125),
+                    products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+                    ]
+
+    # Create promotion catalog
+    second_half_price = promotions.SecondHalfPrice("Second Half price!")
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+
+    # Add promotions to products
+    product_list[0].promotion = second_half_price
+    product_list[1].promotion = third_one_free
+    product_list[3].promotion = thirty_percent
 
     best_buy = store.Store(product_list)
     start(best_buy)
